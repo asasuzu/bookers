69caf667e376ec63621de2a8f090a402ed592dc4
asasuzu <133328222+asasuzu@users.noreply.github.com>
2023-06-01 13:41:50 +0000

    work in progress

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,18 @@
 class BooksController < ApplicationController
+  
   def new
+    # @book = Book.new #viewへ渡すためのインスタント変数
   end
-
+  
+  def create
+    #データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params) 
+    #データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    #詳細画面へリダイレクト
+    # redirect_to book_path(book.id)
+  end
+  
   def index
   end
 
@@ -10,4 +21,12 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  #ストロングパラメータ
+  def book_params
+    params.require(:book) .permit(:title, :body)
+  end
+ 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
- 
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
+  
   root to: 'homes#top' 
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# resources :books で以下のルーティングが自動生成される
+#        root GET    /                          homes#top
+#       books GET    /books(.:format)           books#index
+#             POST   /books(.:format)           books#create
+#    new_book GET    /books/new(.:format)       books#new
+#   edit_book GET    /books/:id/edit(.:format)  books#edit
+#        book GET    /books/:id(.:format)       books#show
+#             PATCH  /books/:id(.:format)       books#update
+#             PUT    /books/:id(.:format)       books#update
+#             DELETE /books/:id(.:format)       books#destroy
